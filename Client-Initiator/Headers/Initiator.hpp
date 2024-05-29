ecf30a1947fe27936e1fb6b7c5745ee425f13f65
Praveen <praveennarayanan01@gmail.com>
2024-05-29 22:50:51 +0530

    added application class methods

--- Client-Initiator/Headers/Initiator.hpp
+++ Client-Initiator/Headers/Initiator.hpp
@@ -3,6 +3,39 @@
 #include <quickfix/MessageCracker.h>
 #include <quickfix/Message.h>
 
-class FIX::Application : public FIX::MessageCracker {
-    
+class MyInitiator : FIX::Application, FIX::MessageCracker {
+
+/// @brief will automatically invoke when session will created to the counter-party
+/// @param session_id  "session ID" refers to a unique identifier representing a specific communication 
+///                     session between two FIX protocol participants.
+void onCreate(const FIX::SessionID &session_id);
+
+/// @brief will automatically invoke when the session logon into the counter-party
+/// @param session_id  "session ID" refers to a unique identifier representing a specific communication 
+///                     session between two FIX protocol participants.
+void onLogon( const FIX::SessionID &session_id);
+
+/// @brief will automatically invoke when the session logout from the counter-party
+/// @param session_id  "session ID" refers to a unique identifier representing a specific communication 
+///                     session between two FIX protocol participants.
+void onLogout( const FIX::SessionID &session_id);
+
+/// @brief will automatically invoke when adminstrative messages sent from our client-side
+/// @param message  FIX::Message
+/// @param session_id FIX::SessionID 
+void toAdmin( FIX::Message &message, const FIX::SessionID &session_id );
+
+/// @brief will automatically invoke when 
+/// @param message FIX::Message
+/// @param session_id FIX::SessionID
+/// @return void 
+void toApp( FIX::Message &message, const FIX::SessionID &session_id ) 
+    throw (FIX::DoNotSend); 
+
+void fromAdmin( const FIX::Message &message, const FIX::SessionID &session_id ) 
+    throw ( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon );
+
+void fromApp( const FIX::Message &message, const FIX::SessionID &session_id ) 
+    throw ( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType );
+ 
 }; 
--- Client-Initiator/Sources/Initiator.cpp
+++ Client-Initiator/Sources/Initiator.cpp
@@ -1,5 +1,38 @@
-#include <Initiator.hpp>
+#include "Headers/Initiator.hpp"
 
-int main() {
-    
+void onCreate(const FIX::SessionID &session_id) {
+  //log - > session has been created!
+}
+
+
+void onLogon( const FIX::SessionID &session_id) {
+  std::cout << "Logon into the session : " << session_id << std::endl;
+  //log- > logon to the session - session id
+  //may sent the subscribtion initially one time here
+}
+
+void onLogout( const FIX::SessionID &session_id) {
+  std::cout << "Logout from the session : " << session_id << std::endl;
+  //logout from the session - sessionid
+}
+
+void toAdmin( FIX::Message &message, const FIX::SessionID &session_id ) {
+  // log-> sending Administrative Messages like 
+  // Logon (A), Logout (5), Heartbeat (0), Test Request (1), Resend Request (2)
+  // to the counterparty will invoke this method
+}
+
+void toApp( FIX::Message &message, const FIX::SessionID &session_id ) 
+  throw (FIX::DoNotSend) {
+    //log -> sending All application message through this method;
+}
+
+void fromAdmin( const FIX::Message &message, const FIX::SessionID &session_id ) 
+  throw ( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon ) {
+    std::cout << "incoming : " << message << std::endl;
+}
+
+void fromApp( const FIX::Message &message, const FIX::SessionID &session_id ) 
+  throw ( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType ) {
+    std::cout << "incoming : " << message << std::endl;
 }
